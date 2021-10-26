ab65c9429396d583b70469f60b3a4c43160fafe7
molo76 <molo976@gmail.com>
2021-10-26 16:15:00 +0100

    add Global Constant Vars

--- Day_012_Scope/main.py
+++ Day_012_Scope/main.py
@@ -1,20 +1,31 @@
 from art import logo
 from random import randint
 
+EASY_LEVEL = 10
+HARD_LEVEL = 5
+
 def check_number(player_guess, guesses_left):
     guesses_left -= 1
     if player_guess == number:
-        print(f"You guessed correctly, you win! Well done!")
+        print(f"You guessed correctly, the number was {number}. You win! Well done!\n")
         guesses_left = 0
         return guesses_left
     elif player_guess > number:
         print("Too high, you have to choose again!")
-        print(f"You have {guesses_left} guesses left.") 
+        print(f"You have {guesses_left} guesses left.\n") 
         return guesses_left
     else:
         print("Too low, choose again!")
-        print(f"You have {guesses_left} guesses left.")
+        print(f"You have {guesses_left} guesses left.\n")
         return guesses_left
+
+def difficulty_level(player_choice):
+    if player_choice == "easy": 
+        print("You selected 'easy', you have 10 guesses!\n")
+        return EASY_LEVEL
+    else: 
+        print("You selected 'hard', you have 5 guesses!\n")
+        return HARD_LEVEL
 
 play = 'y'
 while play == 'y':
@@ -23,14 +34,7 @@ while play == 'y':
     print("Welcome to Number Wang! The Number Guessing Game!")
     print("I'm thining of a number between 1 and 100.")
     number = randint(1,100)
-    difficulty = input("Choose a difficulty. Type 'easy' or hard: ")
-
-    if difficulty == "easy": 
-        print("You selected 'easy', you have 10 guesses!")
-        guesses = 10
-    else: 
-        print("You selected 'hard', you have 5 guesses!")
-        guesses = 5
+    guesses = difficulty_level(input("Choose a difficulty. Type 'easy' or hard: "))
     
     while guesses > 0:
         guess = int(input("Guess a number: "))
