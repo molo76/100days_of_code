bf3e83063a38c33f0389ee01a5420d066d98e64a
molo76 <molo976@gmail.com>
2021-10-27 19:03:39 +0100

    first bits of Day_014

---
+++ Day_013_Bugs/main.py
@@ -0,0 +1,2 @@
+print("Day 13 is a 'bug hunting / fixing' lession. Fix the problems in code provided")
+print("See https://replit.com/@appbrewery/ and search for 'day-13' for more info")
---
+++ Day_014_Higher_Lower_Game/main.py
@@ -0,0 +1,15 @@
+import art
+from game_data import data
+
+compare_a = 0
+compare_b = 0
+
+play = 'yes'
+
+
+print(len(data))
+
+print(data[0])
+print(art.logo)
+
+print(art.vs)
