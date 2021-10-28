d6d950071a3e141fb295305b46f712ea9482bf33
molo76 <molo976@gmail.com>
2021-10-28 19:27:33 +0100

    refactored select_datasets

--- Day_014_Higher_Lower_Game/main.py
+++ Day_014_Higher_Lower_Game/main.py
@@ -1,16 +1,20 @@
 import art
+import random
 from random import randint
 from game_data import data
 
-def select_datasets(already_used):
-    first = randint(0,49)
-    second = randint(0,49)
-    while first in already_used:
-        first = randint(0,49)
-    while second in already_used or second == first:
-        second = randint(0,49)
+def select_datasets(already_used, previous_winner):
+    if current_score == 0:
+        first = random.choice(data)
+        second = random.choice(data)
+    else:
+        first = previous_winner
+        second = random.choice(data)
+    while first["name"] in already_used:
+        first = random.choice(data)
+    while second["name"] in already_used or second["name"] == first["name"]:
+        second = random.choice(data)
     return [first,second]
-
 
 def comparison(compare_a, compare_b):
     if compare_a['follower_count'] > compare_b['follower_count']:
@@ -18,39 +22,43 @@ def comparison(compare_a, compare_b):
     if compare_a['follower_count'] < compare_b['follower_count']:
         return compare_b['name'], compare_b['follower_count']
 
-
-
-used_datasets = []
-datasets = select_datasets(used_datasets)
-first_dataset = data[datasets[0]]
-second_dataset = data[datasets[1]]
-used_datasets.append(datasets)
+play = True
 current_score = 0
+used_datasets = []
+first_dataset = {}
+second_dataset = {}
+
+
+while play == True:
+    datasets = select_datasets(used_datasets, second_dataset)
+    first_dataset = datasets[0]
+    second_dataset = datasets[1]
 
 #debug here: 
-print(first_dataset)
-print(second_dataset)
-
-print(f"Compare A: {first_dataset['name']}, {first_dataset['description']}, {first_dataset['country']}")
-print(f"Against B: {second_dataset['name']}, {second_dataset['description']}, {second_dataset['country']}")
-
-winner = comparison(first_dataset, second_dataset)[0]
-print(winner)
-
-choice = input("Who has more instragram followers, select 'a' or 'b': ")
-if choice == 'a':
-    choice = first_dataset['name']
-elif choice == 'b':
-    choice = second_dataset['name']
-else: 
-    print("You didnt type 'a' or 'b', you are the loser here")
-
-if choice == winner:
-    current_score += 1
-    print(f"You're right, current score = {current_score}")
-else: 
-    print(f"Sorry you're wrong. Final score = {current_score}")
-
+    # print(first_dataset)
+    # print(second_dataset)
+    
+    print(f"Compare A: {first_dataset['name']}, {first_dataset['description']}, {first_dataset['country']}")
+    print(f"Against B: {second_dataset['name']}, {second_dataset['description']}, {second_dataset['country']}")
+    
+    winner = comparison(first_dataset, second_dataset)[0]
+    print(winner)
+    
+    choice = input("Who has more instragram followers, select 'a' or 'b': ")
+    if choice == 'a':
+        choice = first_dataset['name']
+    elif choice == 'b':
+        choice = second_dataset['name']
+    else: 
+        print("You didnt type 'a' or 'b', you are the loser here")
+    
+    if choice == winner:
+        current_score += 1
+        print(f"You're right, current score = {current_score}")
+    else: 
+        print(f"Sorry you're wrong. Final score = {current_score}")
+        play = False
+    
 # print(first_dataset)
 # print(second_dataset)
 # print(used_datasets)
