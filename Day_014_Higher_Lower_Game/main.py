28e4032a5628467b0464e812a0bb213a2feae209
molo76 <molo976@gmail.com>
2021-10-28 10:26:01 +0100

    select unique datasets

--- Day_014_Higher_Lower_Game/main.py
+++ Day_014_Higher_Lower_Game/main.py
@@ -1,15 +1,37 @@
 import art
+from random import randint
 from game_data import data
 
-compare_a = 0
-compare_b = 0
+def select_datasets(already_used):
+    first = randint(0,49)
+    second = randint(0,49)
+    while first in already_used:
+        first = randint(0,49)
+    while second in already_used or second == first:
+        second = randint(0,49)
+    return [first,second]
+
+used_datasets = []
+datasets = select_datasets(used_datasets)
+first_dataset = data[datasets[0]]
+second_dataset = data[datasets[1]]
+used_datasets.append(datasets)
+
+
+print(first_dataset)
+print(second_dataset)
+print(used_datasets)
 
 play = 'yes'
 
+#print(art.logo)
 
-print(len(data))
+#print(art.vs)
 
-print(data[0])
-print(art.logo)
 
-print(art.vs)
+    # {
+    #     'name': 'Instagram',
+    #     'follower_count': 346,
+    #     'description': 'Social media platform',
+    #     'country': 'United States'
+    # },
