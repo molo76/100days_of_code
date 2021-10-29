0ed7fac5f5aadbf46c1b05e642da7aeb1e9b8dbe
molo76 <molo976@gmail.com>
2021-10-29 09:59:05 +0100

    finished

--- Day_014_Higher_Lower_Game/main.py
+++ Day_014_Higher_Lower_Game/main.py
@@ -28,51 +28,31 @@ used_datasets = []
 first_dataset = {}
 second_dataset = {}
 
-
 while play == True:
     datasets = select_datasets(used_datasets, second_dataset)
     first_dataset = datasets[0]
     second_dataset = datasets[1]
 
-#debug here: 
-    # print(first_dataset)
-    # print(second_dataset)
-    
-    print(f"Compare A: {first_dataset['name']}, {first_dataset['description']}, {first_dataset['country']}")
-    print(f"Against B: {second_dataset['name']}, {second_dataset['description']}, {second_dataset['country']}")
+    print(art.logo)
+    if current_score > 0:
+        print(f"You're right, current score = {current_score}")
+    print(f"\nCompare A: {first_dataset['name']}, {first_dataset['description']}, {first_dataset['country']}")
+    print(art.vs)
+    print(f"\nAgainst B: {second_dataset['name']}, {second_dataset['description']}, {second_dataset['country']}")
     
     winner = comparison(first_dataset, second_dataset)[0]
-    print(winner)
+    # print(winner)
     
-    choice = input("Who has more instragram followers, select 'a' or 'b': ")
-    if choice == 'a':
+    choice = input("Who has more instragram followers, type 'A' or 'B': ").upper()
+    if choice == 'A':
         choice = first_dataset['name']
-    elif choice == 'b':
+    elif choice == 'B':
         choice = second_dataset['name']
     else: 
-        print("You didnt type 'a' or 'b', you are the loser here")
+        print("You didnt type 'A' or 'B', you are the loser here")
     
     if choice == winner:
         current_score += 1
-        print(f"You're right, current score = {current_score}")
     else: 
         print(f"Sorry you're wrong. Final score = {current_score}")
         play = False
-    
-# print(first_dataset)
-# print(second_dataset)
-# print(used_datasets)
-
-play = 'yes'
-
-#print(art.logo)
-
-#print(art.vs)
-
-
-    # {
-    #     'name': 'Instagram',
-    #     'follower_count': 346,
-    #     'description': 'Social media platform',
-    #     'country': 'United States'
-    # },
