9b0087289fd9e46c27c3f858d0620863dcbdb1e1
molo76 <molo976@gmail.com>
2021-11-13 14:32:03 +0000

    turtle racing 1.0

--- Day_019_More_Turtles/turtle_race.py
+++ Day_019_More_Turtles/turtle_race.py
@@ -9,35 +9,50 @@ t3 = Turtle()
 t4 = Turtle()
 t5 = Turtle()
 t6 = Turtle()
+finish_line = Turtle()
 
 turtles = [t1, t2, t3, t4, t5, t6]
 colours = ["red", "green", "blue", "orange", "brown", "purple"]
+
+def create_finish_line():
+    finish_line.speed(3)
+    finish_line.hideturtle()
+    finish_line.pu()
+    finish_line.setpos(250, 200)
+    finish_line.setheading(270)
+    finish_line.pd()
+    finish_line.fd(350)
 
 
 def initialise_turtles():
     y_cord = -150
     for turtle in turtles:
+        turtle.shape('turtle')
         y_cord += 50
         turtle.pu()
-        turtle.shape('turtle')
         chosen_colour = random.choice(colours)
         colours.remove(chosen_colour)
         turtle.color(chosen_colour)
         turtle.setpos(-300, y_cord)
 
-initialise_turtles()
 
 def race():
+    race_in_progress = True
     for racer in turtles:
         racer.speed(random.randint(0, 10))
-    while True:
+    while race_in_progress:
         for racer in turtles:
-            racer.fd(10)
+            if racer.xcor() % 5 == 0:
+                racer.speed(random.randint(0, 10))
+            distance = random.randint(0, 25)
+            racer.fd(distance)
+            if racer.xcor() > 250:
+                race_in_progress = False
 
+
+create_finish_line()
+initialise_turtles()
 for turtle in turtles:
     screen.listen()
     screen.onkey(key="space", fun=race)
-
-
-
 screen.exitonclick()
