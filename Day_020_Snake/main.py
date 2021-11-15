221514e45e9129def4a8bb7626ee4879295af130
molo76 <molo976@gmail.com>
2021-11-15 22:22:49 +0000

    snake 0.5

---
+++ Day_020_Snake/main.py
@@ -0,0 +1,26 @@
+from turtle import Screen
+from snake import Snake
+import time
+import random
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title('Nokia 3310 Snake Game')
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_running = True
+while game_running:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+screen.exitonclick()
