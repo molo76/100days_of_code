9f303c4d3464cf11d2ac621a957b333ca402045d
Martyn Jones <matrtyn.jones@pega.com>
2021-11-17 09:16:43 +0000

    some inheritance

---
+++ Day_020_Snake/class_inheritance.py
@@ -0,0 +1,22 @@
+class Animal:
+    def __init__(self):
+        self.num_eyes = 2
+        
+    def breathe(self):
+        print("Inhale, exhale...")
+        
+
+class Fish(Animal):
+    def __init__(self):
+        super().__init__()
+    
+    def breathe(self):
+        super().breathe()
+        print('While underwater')
+
+    def swim(self):
+        print('Fish is swimming.')
+
+nemo = Fish()
+nemo.swim()
+nemo.breathe()
