bb0ce064e2037238efc62c5897d56c8500a051f0
Martyn Jones <matrtyn.jones@pega.com>
2022-01-06 14:10:28 +0000

    us states game completed

--- Day_025_CSV_Pandas/us-states-game/main.py
+++ Day_025_CSV_Pandas/us-states-game/main.py
@@ -8,11 +8,31 @@ image = 'blank_states_img.gif'
 screen.addshape(image)
 turtle.shape(image)
 states_guessed = []
+states_missed = []
 data = pd.read_csv('50_states.csv')
 states = data.state.to_list()
 
 while len(states_guessed) < 50:
     answer_state = screen.textinput(title=f'{len(states_guessed)}/50 - Guess the state', prompt='Enter a state').title()
+    if answer_state == 'Exit':
+        for state in states:
+            if state not in states_guessed:
+                states_missed.append(state)
+
+        if len(states_missed) > 0:
+            output_dict = {
+                'state': [],
+                'x': [],
+                'y': []
+            }
+            for state in states_missed:
+                state_data = data[data.state == state]
+                output_dict['state'] += [state]
+                output_dict['x'] += [state_data.x.item()]
+                output_dict['y'] += [state_data.y.item()]
+
+            output_data = pd.DataFrame(output_dict)
+            output_data.to_csv('missed_states.csv')
     if answer_state in states:
         states_guessed.append(answer_state)
         state = turtle.Turtle()
@@ -33,5 +53,10 @@ if states_guessed == 50:
     completed.penup()
     completed.hideturtle()
     completed.write('You completed the game. Well done!', align='center', font=("Arial", 24, "bold"))
+else:
+    completed = turtle.Turtle()
+    completed.penup()
+    completed.hideturtle()
+    completed.write("You didn't complete the game, try again!", align='center', font=("Arial", 24, "bold"))
 
 screen.exitonclick()
