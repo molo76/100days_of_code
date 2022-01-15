d8d843aa121bfa50c35acbe1a798fa48e0454beb
molo76 <molo976@gmail.com>
2022-01-15 09:33:53 +0000

    add radio button to km/miles converter

--- Day_027_TKinter_args_kwargs/Mile_KM_converter.py
+++ Day_027_TKinter_args_kwargs/Mile_KM_converter.py
@@ -1,39 +1,63 @@
 from tkinter import *
 
+
 def calculate():
+    if radio_state.get() == 1:
+        km_to_miles()
+    elif radio_state.get() == 2:
+        miles_to_km()
+    else:
+        pass
+
+
+def miles_to_km():
+    top_label.config(text='Miles')
+    bottom_label.config(text='Km')
     result_label.config(text=round(float(text_input.get()) * 1.6, 2))
+
+
+def km_to_miles():
+    top_label.config(text='Km')
+    bottom_label.config(text='Miles')
+    result_label.config(text=round(float(text_input.get()) / 1.6, 2))
+
 
 window = Tk()
 window.title('Mile / KM Converter')
 window.minsize(width=300, height=200)
 window.config(padx=10, pady=10)
 
-
 empty_label = Label()
-empty_label.grid(column=0, row=0)
+empty_label.grid(column=0, row=2)
 empty_label.config(padx=40, pady=0)
 
 text_input = Entry(width=10, text='0')
 text_input.insert(END, string="0")
-text_input.grid(column=1, row=0)
+text_input.grid(column=1, row=2)
 
-miles_label = Label(text='Miles')
-miles_label.grid(column=2, row=0)
+top_label = Label(text='Miles')
+top_label.grid(column=2, row=2)
 
 is_equal_label = Label(text='is equal to')
-is_equal_label.grid(column=0, row=1)
+is_equal_label.grid(column=0, row=3)
 
 result_label = Label(text=0)
-result_label.grid(column=1, row=1)
+result_label.grid(column=1, row=3)
 
-km_label = Label(text='Km')
-km_label.grid(column=2, row=1)
+bottom_label = Label(text='Km')
+bottom_label.grid(column=2, row=3)
 
 empty_label2 = Label()
-empty_label.grid(column=0, row=3)
+empty_label.grid(column=0, row=5)
 empty_label.config(padx=40, pady=0)
 
 button = Button(text='Calculate', command=calculate)
-button.grid(column=1, row=2)
+button.grid(column=1, row=4)
+
+radio_state = IntVar()
+radiobutton1 = Radiobutton(text="Km to Miles", value=1, variable=radio_state, command=km_to_miles)
+radiobutton2 = Radiobutton(text="Miles to Km", value=2, variable=radio_state, command=miles_to_km)
+radiobutton1.grid(column=0, row=0)
+radiobutton2.grid(column=0, row=1)
 
 window.mainloop()
