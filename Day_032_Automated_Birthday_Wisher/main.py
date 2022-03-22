4f1cbf14ef8041f8aa06ce99bf5d9547a7964466
molo76 <molo976@gmail.com>
2022-03-22 14:36:24 +0000

    Added Monday_Quotes

--- Day_032_Automated_Birthday_Wisher/main.py
+++ Day_032_Automated_Birthday_Wisher/main.py
@@ -1,19 +1,8 @@
-import smtplib, ssl, email_creds, datetime
+import smtplib, ssl, email_creds
+import datetime as dt
 from random import choice
 
-port = 587
-smtp_server = email_creds.smtp_server
-sender_email = email_creds.sender_email
-password = email_creds.password
-receiver_email = email_creds.receiver_email
-context = ssl.create_default_context()
-
-# message = """\
-# Subject: 100 Days of Code, day 32.
-#
-# This message is sent from Python."""
-
-
+ 
 def pick_quote():
     with open('monday_quotes.txt') as f:
         quotes = f.read()
@@ -36,5 +25,23 @@ def send_mail(mail_from, mail_to, mail_content):
         server.quit()
 
 
-print(pick_quote())
-print(pick_quote())
+port = 587
+smtp_server = email_creds.smtp_server
+sender_email = email_creds.sender_email
+password = email_creds.password
+receiver_email = email_creds.receiver_email
+context = ssl.create_default_context()
+today = dt.datetime.now()
+
+if today.strftime("%A") == 'Monday':
+    quote = pick_quote()
+    SUBJECT = 'Todays words of wisdom'
+    MESSAGE = quote
+    message = f'Subject: {SUBJECT}\n\n{MESSAGE}'
+    print(quote)
+    send_mail(sender_email, receiver_email, message)
+
+
+
+
+
